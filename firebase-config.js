d92b06246b08959f2d35c017529cc845dd3c4dfa
HACorporation <150989193+HACorporation@users.noreply.github.com>
2026-01-20 12:04:44 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,5 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
   const firebaseConfig = {
@@ -16,5 +14,6 @@
 
   // Initialize Firebase
   export const app = initializeApp(firebaseConfig); // Inicilizar Firebase con la configuracion de tu proyecto
+
 
 
