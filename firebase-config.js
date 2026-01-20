3722b9dbd87da42b477f4bb7f4b084a947b19080
HACorporation <150989193+HACorporation@users.noreply.github.com>
2026-01-20 12:10:10 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,16 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    
+const firebaseConfig = {
     apiKey: "AIzaSyDgL66IxKgHYFm6r6Uo20g5TWcta6Rq-3k",
     authDomain: "login-15534.firebaseapp.com",
     projectId: "login-15534",
     storageBucket: "login-15534.firebasestorage.app",
     messagingSenderId: "706532564485",
     appId: "1:706532564485:web:d098e34458c889ee13866c"
-    };
+};
 
-  // Initialize Firebase
-  export const app = initializeApp(firebaseConfig); // Inicilizar Firebase con la configuracion de tu proyecto
+export const app = initializeApp(firebaseConfig);
+
 
 
 
