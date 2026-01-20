2dfe02243664452a5eecfe8981e045a4d807f9c9
HACorporation <150989193+HACorporation@users.noreply.github.com>
2026-01-20 12:18:25 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDgL66IxKgHYFm6r6Uo20g5TWcta6Rq-3k",
@@ -9,8 +10,8 @@ const firebaseConfig = {
     appId: "1:706532564485:web:d098e34458c889ee13866c"
 };
 
-export const app = initializeApp(firebaseConfig);
+// Inicializamos la App
+const app = initializeApp(firebaseConfig);
 
-
-
-
+// Inicializamos y exportamos Auth directamente desde aquí para evitar el error de registro
+export const auth = getAuth(app);
