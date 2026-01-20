6c174780e2e7c213567b8b2e665f9396626a91ca
HACorporation <150989193+HACorporation@users.noreply.github.com>
2026-01-20 11:49:19 -0500

    Update auth-guard.js

--- auth-guard.js
+++ auth-guard.js
@@ -1,5 +1,6 @@
 import { app } from "./firebase-config.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const auth = getAuth(app);
 
@@ -11,3 +12,11 @@ onAuthStateChanged(auth, (user) => {
     window.location.href = "login.html";
   }
 });
+
+document.getElementById("logout").onclick = (e) => {
+    e.preventDefault();
+    const auth = getAuth(app);
+    signOut(auth).then(() => {
+        window.location.href = "login.html";
+    });
+};
